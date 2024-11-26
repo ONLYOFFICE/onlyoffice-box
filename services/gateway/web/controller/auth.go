48a718df879f760f747e6075258549fd6b3733cc
Dmitrii Vershinin <dmitry.vershinin@onlyoffice.com>
2024-11-26 21:13:43 +0300

    refactor(gateway): auth controller

--- services/gateway/web/controller/auth.go
+++ services/gateway/web/controller/auth.go
@@ -15,7 +15,6 @@
  * limitations under the License.
  *
  */
-
 package controller
 
 import (
@@ -77,161 +76,137 @@ func NewAuthController(
 	}
 }
 
-func (c AuthController) getRedirectURL(rw http.ResponseWriter, r *http.Request) string {
-	session, _ := c.session.Get(r, "url")
-	url := "https://app.box.com"
+func (c AuthController) renderErrorPage(rw http.ResponseWriter, errMsgs map[string]interface{}, err error) {
+	c.logger.Warnf("rendeting error page due to: %v", err)
+	embeddable.InstallationErrorPage.Execute(rw, errMsgs)
+}
 
-	if val, ok := session.Values["redirect"].(string); ok {
-		url = val
+func (c AuthController) saveSession(rw http.ResponseWriter, r *http.Request, session *sessions.Session, errMsgs map[string]interface{}) error {
+	if err := session.Save(r, rw); err != nil {
+		c.renderErrorPage(rw, errMsgs, err)
+		return err
 	}
 
+	return nil
+}
+
+func (c AuthController) getRedirectURL(rw http.ResponseWriter, r *http.Request) string {
+	session, _ := c.session.Get(r, "url")
+	redirectURL := session.Values["redirect"]
 	session.Options.MaxAge = -1
 	session.Save(r, rw)
 
-	return url
+	if url, ok := redirectURL.(string); ok {
+		return url
+	}
+
+	return "https://app.box.com"
 }
 
 func (c AuthController) BuildGetAuth() http.HandlerFunc {
 	return func(rw http.ResponseWriter, r *http.Request) {
 		rw.Header().Set("Content-Type", "text/html")
-		v, _ := cv.CreateCodeVerifier()
-		verifier := v.String()
+
+		verifier, _ := cv.CreateCodeVerifier()
+		state, err := c.stateGenerator.GenerateState(verifier.String())
 		errMsgs := map[string]interface{}{
 			"errorMain":    "Installation Failed",
 			"errorSubtext": "Please try again or contact admin",
 			"closeButton":  "Close",
 		}
 
-		session, _ := c.store.Get(r, "onlyoffice-auth")
-		state, err := c.stateGenerator.GenerateState(verifier)
 		if err != nil {
-			embeddable.InstallationErrorPage.Execute(rw, errMsgs)
+			c.renderErrorPage(rw, errMsgs, err)
 			return
 		}
 
+		session, _ := c.store.Get(r, "onlyoffice-auth")
 		session.Values["state"] = state
-		if err := session.Save(r, rw); err != nil {
-			embeddable.InstallationErrorPage.Execute(rw, errMsgs)
+		if err := c.saveSession(rw, r, session, errMsgs); err != nil {
 			return
 		}
 
-		c.logger.Debug("redirecting to auth page")
-
-		http.Redirect(
-			rw, r,
-			fmt.Sprintf(
-				"https://account.box.com/api/oauth2/authorize?client_id=%s&response_type=code&state=%s",
-				c.oauth.ClientID, url.QueryEscape(state),
-			),
-			http.StatusMovedPermanently,
+		authURL := fmt.Sprintf(
+			"https://account.box.com/api/oauth2/authorize?client_id=%s&response_type=code&state=%s",
+			c.oauth.ClientID, url.QueryEscape(state),
 		)
+
+		http.Redirect(rw, r, authURL, http.StatusFound)
 	}
 }
 
 func (c AuthController) BuildGetRedirect() http.HandlerFunc {
 	return func(rw http.ResponseWriter, r *http.Request) {
+		ctx, cancel := context.WithTimeout(context.Background(), 4*time.Second)
+		defer cancel()
+
 		rw.Header().Set("Content-Type", "text/html")
 		query := r.URL.Query()
 		code, state := query.Get("code"), query.Get("state")
+
 		errMsgs := map[string]interface{}{
 			"errorMain":    "Installation Failed",
 			"errorSubtext": "Please try again or contact admin",
 			"closeButton":  "Close",
 		}
 
-		if code == "" {
-			c.logger.Warn("could not request auth credentials. Invalid authorization code")
-			embeddable.InstallationErrorPage.Execute(rw, errMsgs)
+		if code == "" || state == "" {
+			c.renderErrorPage(rw, errMsgs, fmt.Errorf("missing code or state"))
 			return
 		}
-
-		c.logger.Debugf("auth code is valid: %s", code)
-
-		if state == "" {
-			c.logger.Warn("could not request auth credentials. Invalid state")
-			embeddable.InstallationErrorPage.Execute(rw, errMsgs)
-			return
-		}
-
-		c.logger.Debugf("auth state is not empty: %s", state)
 
 		session, _ := c.store.Get(r, "onlyoffice-auth")
 		sessState, ok := session.Values["state"].(string)
-		if !ok {
-			c.logger.Debug("can't cast session state")
-			http.Redirect(rw, r, "/oauth/install", http.StatusMovedPermanently)
+		if !ok || state != sessState {
+			c.renderErrorPage(rw, errMsgs, fmt.Errorf("state mismatch"))
 			return
 		}
-
-		if state != sessState {
-			c.logger.Debugf("auth state %s doesn't match %s", state, sessState)
-			http.Redirect(rw, r, "/oauth/install", http.StatusMovedPermanently)
-			return
-		}
-
-		c.logger.Debugf("auth state is valid: %s", state)
 
 		session.Options.MaxAge = -1
-		if err := session.Save(r, rw); err != nil {
-			rw.WriteHeader(http.StatusInternalServerError)
-			embeddable.InstallationErrorPage.Execute(rw, errMsgs)
+		if err := c.saveSession(rw, r, session, errMsgs); err != nil {
 			return
 		}
 
-		c.logger.Debugf("auth state %s has been removed", state)
-
-		credentials, err := c.boxClient.
-			GetAuthCredentials(context.Background(), code, c.oauth.ClientID, c.oauth.ClientSecret)
+		credentials, err := c.boxClient.GetAuthCredentials(ctx, code, c.oauth.ClientID, c.oauth.ClientSecret)
 		if err != nil {
-			c.logger.Errorf("could not get user credentials: %s", err.Error())
-			embeddable.InstallationErrorPage.Execute(rw, errMsgs)
+			c.renderErrorPage(rw, errMsgs, err)
 			return
 		}
 
-		user, err := c.boxClient.GetMe(context.Background(), credentials.AccessToken)
+		user, err := c.boxClient.GetMe(ctx, credentials.AccessToken)
 		if err != nil {
-			c.logger.Errorf("could not get user info: %s", err.Error())
-			embeddable.InstallationErrorPage.Execute(rw, errMsgs)
+			c.renderErrorPage(rw, errMsgs, err)
 			return
 		}
 
-		var resp interface{}
-		if err := c.client.Call(
-			r.Context(),
-			c.client.NewRequest(
-				fmt.Sprintf("%s:auth", c.config.Namespace), "UserInsertHandler.InsertUser", request.BoxUser{
-					ID:           user.ID,
-					AccessToken:  credentials.AccessToken,
-					RefreshToken: credentials.RefreshToken,
-					TokenType:    credentials.TokenType,
-					ExpiresIn:    credentials.ExpiresIn,
-				},
-			), &resp, client.WithRetries(3)); err != nil {
-			c.logger.Errorf("could not insert a new user: %s", err.Error())
-			embeddable.InstallationErrorPage.Execute(rw, errMsgs)
+		req := c.client.NewRequest(fmt.Sprintf("%s:auth", c.config.Namespace), "UserInsertHandler.InsertUser", request.BoxUser{
+			ID:           user.ID,
+			AccessToken:  credentials.AccessToken,
+			RefreshToken: credentials.RefreshToken,
+			TokenType:    credentials.TokenType,
+			ExpiresIn:    credentials.ExpiresIn,
+		})
+		if err := c.client.Call(ctx, req, nil, client.WithRetries(3)); err != nil {
+			c.renderErrorPage(rw, errMsgs, err)
 			return
 		}
 
-		signature, err := c.jwtManager.Sign(c.oauth.ClientSecret, jwt.RegisteredClaims{
+		jwtToken, err := c.jwtManager.Sign(c.oauth.ClientSecret, jwt.RegisteredClaims{
 			ID:        user.ID,
 			ExpiresAt: jwt.NewNumericDate(time.Now().Add(7 * 24 * time.Hour)),
 		})
-
 		if err != nil {
-			c.logger.Errorf("could not issue a new jwt: %s", err.Error())
-			embeddable.InstallationErrorPage.Execute(rw, errMsgs)
+			c.renderErrorPage(rw, errMsgs, err)
 			return
 		}
 
-		session.Values["token"] = signature
+		session.Values["token"] = jwtToken
 		session.Values["locale"] = user.Language
 		session.Options.MaxAge = 60 * 60 * 23 * 7
-		if err := session.Save(r, rw); err != nil {
-			c.logger.Errorf("could not save a new session cookie: %s", err.Error())
-			embeddable.InstallationErrorPage.Execute(rw, errMsgs)
+		if err := c.saveSession(rw, r, session, errMsgs); err != nil {
 			return
 		}
 
-		http.Redirect(rw, r, c.getRedirectURL(rw, r), http.StatusMovedPermanently)
+		http.Redirect(rw, r, c.getRedirectURL(rw, r), http.StatusFound)
 	}
 }
